98a2c26436885cc9a5f53dd1767ecde90a95ad57
huhjihye <floramiss0700@gmail.com>
2023-01-23 23:44:08 +0900

    20230123 인프런 스프링 핵심 원리-기본편

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -37,7 +37,7 @@ public class HelloController {
 
     //  @ResponseBody를 사용
     // - HTTP의 BODY에 문자 내용을 직접 반환
-    // - 'viewResolver' 대신에 'HttpMessageConverter'가 동작
+    // - 'viewResolver' 대신에 'HttpMessageConverter'가 동작..
     // - 기본 문자처리 : 'StringHttpMessageConverter'
     // - 기본 객체처리 : 'MappingJackson2HttpMessageConverter'
     // - byte 처리 등등 기타 여러 HttpMessageConverter가 기본으로 등록되어 있음
